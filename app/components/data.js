b3ecaa9b1378aa27e759e8571af3e76bca9741ac
ricky spires <rickyspires@hotmail.com>
2019-01-12 15:59:11 +0000

    first commit

---
+++ app/components/data.js
@@ -0,0 +1,7 @@
+const contactData = [
+  { id: 1, name: 'jeff spires', email: 'jeff@spires.com' },
+  { id: 2, name: 'terry spires', email: 'terry@spires.com' },
+  { id: 3, name: 'ricky spires', email: 'ricky@spires.com' },
+];
+
+export default contactData;
